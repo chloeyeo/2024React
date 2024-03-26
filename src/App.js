cc23237b961fa12a400cb77ca10239523a505bcc
Chloe Yeo <yeochloe01@gmail.com>
2024-03-26 16:13:36 +0900

    react style practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,40 @@
 import "./App.css";
+
+function Child(props) {
+  const text = "this is a child";
+  return (
+    <>
+      <p>{text}</p>
+      {props.handler()}
+    </>
+  );
+}
 function App() {
   const welcome = "hi";
-  return <h1 className="title">{welcome}</h1>;
+  const myCss = {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    color: "blue",
+  };
+  function handleFunc() {
+    const text = "inside handle function";
+    return <p>{text}</p>;
+  }
+  return (
+    // <div
+    //   style={{
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     alignItems: "center",
+    //     color: "blue",
+    //   }}
+    // >
+    <div style={myCss}>
+      <h1 className="title">{welcome}</h1>
+      <Child handler={handleFunc} />
+    </div>
+  );
 }
 
 export default App;
